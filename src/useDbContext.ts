7de8575f3eb716b442793d97b3cd0a378b0e96bb
Mykhailo Skrypskyi <mixator@gmail.com>
2025-05-07 14:18:31 +0300

    feat: add pg

---
+++ src/useDbContext.ts
@@ -0,0 +1,10 @@
+import { pg } from "pg";
+
+const client = new pg.Client({
+  user: "bob",
+  host: "database.server.com",
+  database: "mydb",
+  password: "correct-horse-battery-staple",
+  port: 3211,
+});
+client.connect();
